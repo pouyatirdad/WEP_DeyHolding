956d5af552f7811e42f0b5047efe8946421bd93b
Kamyab <Kamyab@REVISION-PC>
2021-10-27 10:00:21 +0330

    Racaptcha

---
+++ Road.Web/Recaptcha/SiteSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Road.Web.Recaptcha
+{
+    public class SiteSettings
+    {
+        public const string GoogleRecaptchaSecretKey = "6LfsGfgcAAAAAJq0ep3HooXDjvK5l78ljrrvz-3S";
+        public const string GoogleRecaptchaSiteKey = "6LfsGfgcAAAAAIejMkRM2wuGNZqbchM4IX1Wu5sB";
+    }
+}
