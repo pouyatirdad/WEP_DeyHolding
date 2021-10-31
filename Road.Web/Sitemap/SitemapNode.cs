e867494f0a4c72240ae748ed5cda872ef9f8e983
Kamyab <Kamyab@REVISION-PC>
2021-10-31 12:36:45 +0330

    Sitemap

---
+++ Road.Web/Sitemap/SitemapNode.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Road.Web.Sitemap
+{
+    public class SitemapNode
+    {
+        public SitemapFrequency? Frequency { get; set; }
+        public DateTime? LastModified { get; set; }
+        public double? Priority { get; set; }
+        public string Url { get; set; }
+    }
+
+    public enum SitemapFrequency
+    {
+        Never,
+        Yearly,
+        Monthly,
+        Weekly,
+        Daily,
+        Hourly,
+        Always
+    }
+
+
+
+    public static class UrlHelperExtensions
+    {
+        public static string AbsoluteRouteUrl(
+            this UrlHelper urlHelper,
+            string routeName,
+            object routeValues = null)
+        {
+            string scheme = urlHelper.RequestContext.HttpContext.Request.Url.Scheme;
+            return urlHelper.RouteUrl(routeName, routeValues, scheme);
+        }
+    }
+}
